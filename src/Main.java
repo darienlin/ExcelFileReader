8a8d444f03d4e16c8e416e839db0c1cb88de90f5
darienlin <darienlin87@gmail.com>
2022-10-03 19:05:09 -0700

    can read sku and image column. also can remove duplicates and add the other small images

--- src/Main.java
+++ src/Main.java
@@ -29,36 +29,72 @@ public class Main {
         //}
 
         Sheet sheet = wb.getSheet("Sheet1");
-        int rowIndex = 1; //as you go higher goes down column
+        int rowIndex = 1;//as you go higher goes down column
+        int row2Index;
         Row row = sheet.getRow(rowIndex - 1);
         Row row1 = sheet.getRow(rowIndex);
+        Row imageRow;
         int cellIndex = 0;//0 is skuName       1 is image
         String skuName = String.valueOf(row.getCell(rowIndex - 1));
         String skuName1 = String.valueOf(row.getCell(rowIndex));
+        String xImages = String.valueOf(row.getCell(rowIndex));
+        String allXImages ="";
+        String picNow;
         int rowCount = sheet.getLastRowNum()+1;
+        int xImageCounter = 0;
+        boolean first = false, ploop = true;
 
         while( rowIndex < rowCount) {
             row = sheet.getRow(rowIndex - 1);
             row1 = sheet.getRow(rowIndex);
-            skuName = String.valueOf(row.getCell(cellIndex));
-            skuName1 = String.valueOf(row1.getCell(cellIndex));
+            skuName = String.valueOf(row.getCell(0));
+            skuName1 = String.valueOf(row1.getCell(0));
 
             //System.out.println(skuName + " " + skuName1 + "\n" + skuName.equals(skuName1));
 
             if(skuName.equals(skuName1)) {
                 rowIndex++;
+                xImageCounter++;
+
+                if(first){
+                    row2Index = rowIndex - 1;
+                    first = false;
+                    for(int i = 1; i < xImageCounter; i++){
+                        imageRow = sheet.getRow(i);
+                        xImages = String.valueOf(imageRow.getCell(1));
+                        xImages = xImages.substring(0,xImages.indexOf(".jpg") + 4);
+                        allXImages = allXImages + "," + xImages;
+                    }
+                }
+
+                else
                 continue;
             }
 
             else {
-                skuNameList.add(String.valueOf(row.getCell(cellIndex)));
+                skuNameList.add(String.valueOf(row1.getCell(0)));
+                picNow = String.valueOf(row1.getCell(1));
+                picNow = picNow.substring(0,picNow.indexOf(".jpg") + 4);
+                picNameList.add(picNow + allXImages);
                 rowIndex++;
+
+                if(ploop){
+                    first = true;
+                    ploop = false;
+                }
             }
         }
 
+        row1 = sheet.getRow(0);
+        skuNameList.add(String.valueOf(row1.getCell(0)));
+        picNow = String.valueOf(row1.getCell(1));
+        picNow = picNow.substring(0,picNow.indexOf(".jpg") + 4);
+        picNameList.add(picNow + allXImages);
+
 
         for(int i = 0; i < skuNameList.size(); i++)
-            System.out.println(skuNameList.get(i) + "\n");
+            System.out.println(skuNameList.get(i) +" " + picNameList.get(i) + "\n");
+
 
 
 
